c85be930f7ac4f29f0bb41847a1a19c01ca111e5
Futuramuchi <Yana130302@gmail.com>
2024-01-30 21:40:39 +0300

    adds

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AudioPlayer
+{
+    public static void PlayForAutograph()
+    {
+        
+    }
+    
+    public static void PlayForAroma()
+    {
+        
+    }
+    
+    public static void PlayForLegalBusiness()
+    {
+        
+    }
+    
+    public static void PlayForDocumentalCinema()
+    {
+        
+    }
+    
+    public static void PlayForClubJoin()
+    {
+            
+    }
+    
+    public static void PlayForClubJoin()
+    {
+                
+    }
+    
+    public static void PlayForWatch()
+    {
+                    
+    }
+    
+    public static void PlayForLegalOlympic()
+    {
+                        
+    }
+      
+   public static void PlayForSuccessfulBuild()
+   {
+                        
+   }
+}
